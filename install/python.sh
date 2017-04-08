d3dd91acac6b4646706c09ae432025f2b406600f
Shadow <tristand.pro@gmail.com>
2017-04-08 13:25:28 +0200

    modif

--- install/python.sh
+++ install/python.sh
@@ -1,3 +1,3 @@
 update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
 update-alternatives --install /usr/bin/python python /usr/bin/python3.4 2
-
+update-alternatives --install /usr/bin/python python /usr/bin/python3.6.1 3
